6b03ee92bc50afa8e00198415f79bd9b76739e0a
Berenice Solohaga <bsolohaga@hep.local>
2025-09-16 11:38:24 -0300

    comming title

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Navbar from "@/components/navbar";
+import Introduction from "@/components/introduction";
+import AboutMe from "@/components/about-me";
+
+export default function Home() {
+  return (
+   <main className="pb-40">
+      <Navbar />
+      <Introduction />
+      <AboutMe />
+   </main>
+  );
+}
